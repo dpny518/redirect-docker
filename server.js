d59a0022b680ff470f537c497059c139e37d456d
DP <5721671+dpny518@users.noreply.github.com>
2024-04-18 20:10:11 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+let lastAuthorizationCode = null; // Variable to store the last received authorization code
+
 app.get('/oauth/callback', (req, res) => {
     const authorizationCode = req.query.code;
     if (!authorizationCode) {
@@ -8,7 +10,18 @@ app.get('/oauth/callback', (req, res) => {
         return;
     }
 
+    // Store the received authorization code
+    lastAuthorizationCode = authorizationCode;
     res.send(`Authorization code received: ${authorizationCode}`);
+});
+
+// Endpoint to retrieve the last stored authorization code
+app.get('/getcode', (req, res) => {
+    if (!lastAuthorizationCode) {
+        res.status(404).send('No authorization code available');
+        return;
+    }
+    res.send(`Last Authorization Code: ${lastAuthorizationCode}`);
 });
 
 const port = 8000;
